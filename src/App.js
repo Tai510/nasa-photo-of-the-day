7f6e18a6fa5842a06ddf954c92f4a35683f70f96
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-09-11 15:12:03 -0700

    finished mvp

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import Image from './Components/Image/image.js';
 function App() {
   const [image , setImage] = useState();  
   const [ title, setTitle ] = useState();
+  const [ date, setDate ] = useState();
+  const [ copyright, setCopyright ] = useState();
 
   useEffect(() => {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     .then((res) => {
-        setImage(res.data.hdurl);
+        setImage(res.data.url);
         setTitle(res.data.title);
+        setDate(res.data.date);
+        setCopyright(res.data.copyright);
         return res;  
     })
     .then((res) => {
@@ -26,7 +30,11 @@ function App() {
 
   return (
     <div className="App">
-       <Image src={image} title={title}/>
+      <div className='nasa-icons'>
+      <i class="fas fa-user-astronaut"></i> Nasa <i class="fas fa-rocket"></i>
+      </div>
+    <h1 className='h1'>Photo Of The Day</h1>
+       <Image src={image} title={title} date={date} copyright={copyright} />
     </div>
   );
 }
--- src/Components/Image/image.js
+++ src/Components/Image/image.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 
 export default function Image (props) {
+    console.log(props)
     return (
       <div>
-        <img className='image-style' src={props.hdurl}>
-        </img>
-        <p title={props.title}> <span>Title :</span>  {props.title}</p>
+          <img className='image-style' src={props.url}>
+          </img>
+       <div className='image-section'>
+          <p title={props.title}> <span>Title :</span>  {props.title}</p>
+          <p date={props.date}> {props.date}</p>
+          <p id='copyright' copyright={props.copyright}> Copyright ©  {props.copyright}</p>
+
+       </div>
      </div>
     );
 }
