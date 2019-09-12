9727e1fb497964a8054f216cec5cbbb383463abf
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-09-12 10:45:42 -0700

    day 2 Mvp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,36 @@ import React, {useState , useEffect} from "react";
 import axios from 'axios';
 import "./App.css";
 import Image from './Components/Image/image.js';
+import styled, { css } from 'styled-components'
+
+const MainApp = styled.div`
+text-align: center;
+`;
+
+const H1 = styled.h1`
+  border: 3px solid grey;
+  margin: 0px 508px;
+  height: 50px;
+  padding-top: 5px;
+  background-color: #6D89A6;
+  color: white;
+  text-shadow: 1px 1px black;
+  border-top: 3px solid #6D89A6;
+`;
+
+const NasaIcons = styled.div `
+  border: 3px solid grey;
+  margin: 0 508px;
+  height: 50px;
+  background-color: #6D89A6;
+  color: white;
+  text-shadow: 1px 1px black;
+  border-bottom: 3px solid #6D89A6;
+  font-size: 40px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: bolder;
+`
 
 
 function App() {
@@ -29,13 +59,13 @@ function App() {
 
 
   return (
-    <div className="App">
-      <div className='nasa-icons'>
-      <i class="fas fa-user-astronaut"></i> Nasa <i class="fas fa-rocket"></i>
-      </div>
-    <h1 className='h1'>Photo Of The Day</h1>
-       <Image src={image} title={title} date={date} copyright={copyright} />
-    </div>
+    <MainApp>
+        <NasaIcons>
+          <i class="fas fa-user-astronaut"></i> Nasa <i class="fas fa-rocket"></i>
+        </NasaIcons>
+        <H1>Photo Of The Day</H1>
+        <Image src={image} title={title} date={date} copyright={copyright} />
+    </MainApp>
   );
 }
 
--- src/Components/Image/image.js
+++ src/Components/Image/image.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import styled, { css } from 'styled-components'
+
+const ImageStyle = styled.img`
+height: 420px;
+width: 420px;
+border: 3px solid grey;
+`;
+
+const Span = styled.span`
+font-weight: bolder;
+font-size: 25px
+`;
+
+const Para = styled.p`
+font-weight: bolder;
+font-size: 20px
+`;
+
+const ImageSection = styled.div`
+border: 3px solid grey;
+margin: 5px 508px;
+background-color: #6D89A6;
+color: white;
+text-shadow: 1px 1px black;
+`;
+
+
 
 
 export default function Image (props) {
     console.log(props)
     return (
       <div>
-          <img className='image-style' src={props.url}>
-          </img>
-       <div className='image-section'>
-          <p title={props.title}> <span>Title :</span>  {props.title}</p>
-          <p date={props.date}> {props.date}</p>
-          <p id='copyright' copyright={props.copyright}> Copyright ©  {props.copyright}</p>
-
-       </div>
+          <ImageStyle src={props.url}>
+          </ImageStyle>
+       <ImageSection>
+          <Para title={props.title}> <Span>Title :</Span>  {props.title}</Para>
+          <Para date={props.date}> {props.date}</Para>
+          <Para id='copyright' copyright={props.copyright}> Copyright ©  {props.copyright}</Para>
+       </ImageSection>
      </div>
     );
 }
